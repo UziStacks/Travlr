3fa8efd743786b775f85e64e86a88a6781424d7b
UziStacks <terel.phillips23@gmail.com>
2022-09-29 17:37:00 -0400

    -created and animated burger menu -made nav logo colors dyanmic

--- src/components/home/Nav.component.jsx
+++ src/components/home/Nav.component.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 const Nav = ({ handleModalToggle }) => {
+  const [burgerToggle, setBurgerToggle] = useState("close");
+  const [mobile, setMobile] = useState(false);
+  const toggleBurger = () =>
+    setBurgerToggle(burgerToggle === "close" ? "open" : "close");
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }, [setMobile]);
+
+  const styles = {
+    container: "absolute w-full",
+    subContainer: "flex justify-between text-white mx-6 mt-5",
+    logo: "title text-[46px]",
+    menu: "menu-btn",
+  };
+  const { container, subContainer, logo, menu } = styles;
+
   return (
     <>
-      <nav className="absolute w-full">
-        <div className="flex justify-between text-white mx-6 mt-5">
-          <a href="/" className="title text-[46px]">
+      <nav className={container}>
+        <div className={subContainer}>
+          <a href="/" className={logo}>
             Travlr.
           </a>
-          <button onClick={handleModalToggle}>Get started</button>
+          {mobile ? (
+            <div onClick={toggleBurger} className={menu}>
+              <div className={burgerToggle}></div>
+              <div className={burgerToggle}></div>
+              <div className={burgerToggle}></div>
+            </div>
+          ) : (
+            <button onClick={handleModalToggle}>Get started</button>
+          )}
         </div>
       </nav>
+
       <Outlet />
     </>
   );
